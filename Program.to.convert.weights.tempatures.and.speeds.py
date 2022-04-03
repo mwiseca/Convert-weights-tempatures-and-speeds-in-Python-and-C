2f2eaa8194c582ba1a68870d2081780ba1beb635
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2022-04-02 21:47:22 -0400

    Add files via upload

--- Program.to.convert.weights.tempatures.and.speeds.py
+++ Program.to.convert.weights.tempatures.and.speeds.py
@@ -17,81 +17,118 @@ def ind():
 
 ind()         
 print("Enter i for index or x to exit.")
+print("Enter m to go back to main menu")
 while True:
     try:
-        convert = input("Enter a letter from index you want to convert. i for index ")
+        convert = input("Enter a letter from index you want to convert. i for index  m for main menu ")
         if convert == "c":
-            print("Enter tempature in celsius.")
-            celsius = input()
-            result = float(celsius)  / 5 * 9 + 32
-            print (result)
-            print("fahrenheit")    
+            while True:
+                print("Enter tempature in celsius.")
+                celsius = input()
+                if celsius == "m":
+                    break
+                result = float(celsius)  / 5 * 9 + 32
+                print (result)
+                print("fahrenheit")    
         elif convert == "f":
-            print("Enter a tempature in fahrenheit.")
-            fahrenheit = input()
-            result = ((float(fahrenheit)) - 32) * 5 / 9
-            print (result)
-            print ("celsious")
+            while True:
+                print("Enter a tempature in fahrenheit.")
+                fahrenheit = input()
+                if fahrenheit == "m":
+                    break
+                result = ((float(fahrenheit)) - 32) * 5 / 9
+                print (result)
+                print ("celsious")
         elif convert == "g":
-            print("Enter a weight in grams.")
-            weight_grams = input()
-            weight_lbs = float(weight_grams) *.00220462
-            print(weight_lbs)
-            print("pounds")
+            while True:
+                print("Enter a weight in grams.")
+                weight_grams = input()
+                if weight_grams == "m":
+                    break
+                weight_lbs = float(weight_grams) *.00220462
+                print(weight_lbs)
+                print("pounds")
         elif convert == "pg":
-            print("Enter a weight in pounds.")
-            weight_pounds = input()
-            weight_grams = float(weight_pounds) * 453.59237
-            print(weight_grams)
-            print("grams")
+            while True:
+                print("Enter a weight in pounds.")
+                weight_pounds = input()
+                if weight_pounds == "m":
+                    break
+                weight_grams = float(weight_pounds) * 453.59237
+                print(weight_grams)
+                print("grams")
         elif convert == "lbs":
-            print("Enter a weight in lbs.")
-            weight_lbs = input()
-            weight_kg = float(weight_lbs) * 0.45359237
-            print(weight_kg) 
-            print("kilograms")
+            while True:
+                print("Enter a weight in lbs.")
+                weight_lbs = input()
+                if weight_lbs == "m":
+                    break
+                weight_kg = float(weight_lbs) * 0.45359237
+                print(weight_kg) 
+                print("kilograms")
         elif convert == "kg":
-            print("Enter a weight in kg.")
-            weight_kg = input()
-            weight_lbs = float(weight_kg) * 2.2046226218
-            print(weight_lbs)
-            print("pounds")
+            while True:
+                print("Enter a weight in kg.")
+                weight_kg = input()
+                if weight_kg == "m":
+                    break
+                weight_lbs = float(weight_kg) * 2.2046226218
+                print(weight_lbs)
+                print("pounds")
         elif convert == "oz":
-            print("Enter a weight in ounces.")
-            weight_oz = input()
-            weight_lbs = float(weight_oz) * 0.062500
-            print(weight_lbs)
-            print("pounds")
+            while True:
+                print("Enter a weight in ounces.")
+                weight_oz = input()
+                if weight_oz == "m":
+                    break
+                weight_lbs = float(weight_oz) * 0.062500
+                print(weight_lbs)
+                print("pounds")
         elif convert == "lboz":
-            print("Enter a weight in pounds.")
-            weight_lbs = input()
-            weight_oz = float(weight_lbs) *16
-            print(weight_oz)
-            print("ounces")
+            while True:
+                print("Enter a weight in pounds.")
+                weight_lbs = input()
+                if weight_lbs == "m":
+                    break
+                weight_oz = float(weight_lbs) *16
+                print(weight_oz)
+                print("ounces")
         elif convert == "kl":
-            print("Enter a distance in kilometers.")
-            distance_klm = input()
-            distance_miles = float(distance_klm) * 0.62137119223733
-            print (distance_miles)
-            print("miles")
+            while True:
+                print("Enter a distance in kilometers.")
+                distance_klm = input()
+                if distance_klm == "m":
+                    break
+                distance_miles = float(distance_klm) * 0.62137119223733
+                print (distance_miles)
+                print("miles")
         elif convert == "mk":
-            print("Enter a distance in miles")
-            distance_miles = input()
-            distance_kilometers = float(distance_miles) *1.609344
-            print(distance_kilometers)
-            print("kilometers")
+            while True:
+                print("Enter a distance in miles")
+                distance_miles = input()
+                if distance_miles == "m":
+                    break
+                distance_kilometers = float(distance_miles) *1.609344
+                print(distance_kilometers)
+                print("kilometers")
         elif convert == "mi":
-            print("Enter speed miles per hour.") 
-            speed_mph = input()
-            speed_klm = float(speed_mph) * 1.609344
-            print(speed_klm)
-            print("kilometers per hour")
+            while True:
+                print("Enter speed miles per hour.") 
+                speed_mph = input()
+                if speed_mph == "m":
+                    break
+                speed_klm = float(speed_mph) * 1.609344
+                print(speed_klm)
+                print("kilometers per hour")
         elif convert == "kph":
-            print("Enter a speed in kilometers per hour.")
-            speed_kpm = input()
-            speed_mph = float(speed_kpm) / 1.609344
-            print(speed_mph)
-            print("Miles per hour")
+            while True:
+                print("Enter a speed in kilometers per hour.")
+                speed_kpm = input()
+                if speed_kpm == "m":
+                    break
+                speed_mph = float(speed_kpm) / 1.609344
+                print(speed_mph)
+                print("Miles per hour")
         elif convert == "i":
             ind()
         elif convert == "x":
@@ -107,7 +144,9 @@ while True:
             
     
         
-        
+            
+                
+   
     
         
 
